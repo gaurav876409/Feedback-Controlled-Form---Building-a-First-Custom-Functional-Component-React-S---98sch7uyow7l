6a4a0c475d056b15158cab8a9589fde791123623
gaurav876409 <gaurav876409@gmail.com>
2023-05-23 08:31:27 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,54 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
 
 const App = () => {
-  const [rating, setRating] = useState();
-  const [comment, setComment] = useState();
-  const [commentError, setCommentError] = useState();
+  const [rating, setRating] = useState(5);
+  const [comment, setComment] = useState('');
+  const [commentError, setCommentError] = useState('');
+
+  const handleRatingChange = (event) => {
+    setRating(event.target.value);
+  };
+
+  const handleCommentChange = (event) => {
+    setComment(event.target.value);
+    if (event.target.value.length < 5) {
+      setCommentError('Comment must be at least 5 characters.');
+    } else {
+      setCommentError('');
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <div id="main">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor='rating'>Rating: </label>
-          <input 
+          <label htmlFor="rating">Rating: </label>
+          <input
             type="range"
             min="1"
             max="10"
             id="rating"
+            value={rating}
+            onChange={handleRatingChange}
           />
-          <span className='rating'>rating</span>
+          <span className="rating">{rating}</span>
         </div>
         <div>
-          <label htmlFor='comment'>Comment: </label>
-          <textarea 
-            id='comment'
-          />
-          <p style={{ color: 'red' }} className="comment-error">Comment must be atleast 5 characters.</p>
+          <label htmlFor="comment">Comment: </label>
+          <textarea id="comment" value={comment} onChange={handleCommentChange} />
+          <p style={{ color: 'red' }} className="comment-error">
+            {commentError}
+          </p>
         </div>
-        <button type='submit'>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
